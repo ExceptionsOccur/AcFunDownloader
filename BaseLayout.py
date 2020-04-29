942045f2139ad94413e1fdb54d0d7328b82f7e52
ExceptionsOccur <yuyu.tao@foxmail.com>
2020-04-29 17:37:06 +0800

    modify readme

--- BaseLayout.py
+++ BaseLayout.py
@@ -2,6 +2,7 @@
 import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPalette, QBrush, QPixmap
 
 
 class BaseUI(object):
@@ -10,10 +11,16 @@ class BaseUI(object):
         _translate = QtCore.QCoreApplication.translate
 
         MWin.setObjectName('GUI_MWin')
-        MWin.setFixedSize(400, 150)
+        MWin.setFixedSize(420, 260)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        MWin.setWindowOpacity(0.9)
+        # MWin.setAttribute(QtCore.Qt.WA_TintedBackground)
         # MWin.resize(400, 120)
         # MWin.setMinimumSize(QtCore.QSize(400, 120))
         # MWin.setMaximumSize(QtCore.QSize(600, 120))
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(QPixmap('./ac.png')))
+        MWin.setPalette(palette)
         font = QtGui.QFont()
         font.setFamily('微软雅黑')
         font.setPointSize(10)
@@ -25,7 +32,7 @@ class BaseUI(object):
 
         self.central_widget = QtWidgets.QWidget(MWin)   # create based parent widget
         self.central_widget.setObjectName('central_widget')
-        self.central_widget.resize(400, 150)
+        self.central_widget.resize(420, 260)
 
         self.vertical_layout = QtWidgets.QVBoxLayout(self.central_widget)
         self.vertical_layout.setContentsMargins(10, 10, 10, 10)
@@ -34,6 +41,26 @@ class BaseUI(object):
 
         # self.central_frame = QtWidgets.QFrame(self.central_widget)
         # self.central_frame.setStyleSheet("background-color:green;")
+
+        self.console_layout = QtWidgets.QHBoxLayout()
+        # self.console_layout.setSpacing(10)
+        self.console_layout.setObjectName('console_layout')
+
+        self.close_btn = QtWidgets.QPushButton(self.central_widget)
+        self.close_btn.setObjectName('close_btn')
+        self.close_btn.setFixedSize(18, 18)
+        self.close_btn.setText(_translate('GUI_MWin', 'x'))
+
+        self.mini_btn = QtWidgets.QPushButton(self.central_widget)
+        self.mini_btn.setObjectName('mini_btn')
+        self.mini_btn.setFixedSize(18, 18)
+        self.mini_btn.setText(_translate('GUI_MWin', '-'))
+
+        self.console_layout.addWidget(self.close_btn)
+        self.console_layout.addWidget(self.mini_btn)
+        self.console_layout.addStretch()
+        self.vertical_layout.addLayout(self.console_layout)
+
 
         self.up_layout = QtWidgets.QHBoxLayout()
         self.up_layout.setSpacing(10)
@@ -67,6 +94,9 @@ class BaseUI(object):
         self.download_btn.setObjectName('download_btn')
         self.download_btn.setText(_translate('GUI_MWin', '下载'))
         self.up_layout.addWidget(self.download_btn)
+        # self.up_layout.setStretchFactor(self.input_box, 8)
+        # self.up_layout.setStretchFactor(self.search_btn, 8)
+        # self.up_layout.setStretchFactor(self.download_btn, 2)
         self.vertical_layout.addLayout(self.up_layout)
 
 
@@ -76,12 +106,12 @@ class BaseUI(object):
         self.tips_layout.setSpacing(10)
         self.tips_layout.setObjectName('tips_layout')
 
-        self.download_bar = QtWidgets.QProgressBar(self.central_widget)
-        self.download_bar.setMinimum(0)
-        self.download_bar.setMaximum(100)
-        self.download_bar.setOrientation(QtCore.Qt.Horizontal)
-        self.download_bar.setObjectName('download_bar')
-        self.tips_layout.addWidget(self.download_bar)
+        # self.download_bar = QtWidgets.QProgressBar(self.central_widget)
+        # self.download_bar.setMinimum(0)
+        # self.download_bar.setMaximum(100)
+        # self.download_bar.setOrientation(QtCore.Qt.Horizontal)
+        # self.download_bar.setObjectName('download_bar')
+        # self.tips_layout.addWidget(self.download_bar)
 
         self.fold_btn = QtWidgets.QPushButton(self.central_widget)
         font = QtGui.QFont()
@@ -92,6 +122,27 @@ class BaseUI(object):
         self.fold_btn.setObjectName('fold_btn')
         self.fold_btn.setText(_translate('GUI_MWin', os.getcwd()))
         self.tips_layout.addWidget(self.fold_btn)
+
+        self.pause_btn = QtWidgets.QPushButton(self.central_widget)
+        font = QtGui.QFont()
+        font.setFamily('微软雅黑')
+        font.setPointSize(10)
+        self.pause_btn.setFont(font)
+        self.pause_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pause_btn.setObjectName('fold_btn')
+        self.pause_btn.setText(_translate('GUI_MWin', '暂停'))
+        self.tips_layout.addWidget(self.pause_btn)
+
+        self.cancel_btn = QtWidgets.QPushButton(self.central_widget)
+        font = QtGui.QFont()
+        font.setFamily('微软雅黑')
+        font.setPointSize(10)
+        self.cancel_btn.setFont(font)
+        self.cancel_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.cancel_btn.setObjectName('fold_btn')
+        self.cancel_btn.setText(_translate('GUI_MWin', '取消'))
+        self.tips_layout.addWidget(self.cancel_btn)
+
 
         self.vertical_layout.addLayout(self.tips_layout)
 
@@ -106,6 +157,8 @@ class BaseUI(object):
         self.table_widget.verticalHeader().setVisible(False)
         # self.table_widget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)  # 自动铺满父控件
         self.table_widget.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        # self.table_widget.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.table_widget.setShowGrid(False)
 
         # self.table_widget.setFocusPolicy(QtCore.Qt.StrongFocus)
         # self.table_widget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
@@ -118,16 +171,23 @@ class BaseUI(object):
         # self.table_widget.setGridStyle(QtCore.Qt.SolidLine)
         self.table_widget.setColumnCount(2)
         self.table_widget.setObjectName("table_widget")
-        self.table_widget.setRowCount(2)
+        self.table_widget.setRowCount(4)
         self.vertical_layout.addWidget(self.table_widget)
-        self.up_layout.setStretchFactor(self.input_box, 8)
-        self.up_layout.setStretchFactor(self.search_btn, 2)
-        self.up_layout.setStretchFactor(self.download_btn, 2)
         self.table_widget.setColumnWidth(0, 60)
         self.table_widget.setColumnWidth(1, 318)
         # self.table_widget.setItem(0, 0, QtWidgets.QTableWidgetItem('标题').setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter))
         # self.table_widget.setItem(1, 0, QtWidgets.QTableWidgetItem('up主').setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter))
         self.table_widget.setItem(0, 0, QtWidgets.QTableWidgetItem('标题'))
         self.table_widget.setItem(1, 0, QtWidgets.QTableWidgetItem('up主'))
+        self.table_widget.setItem(2, 0, QtWidgets.QTableWidgetItem('上传时间'))
+        self.table_widget.setItem(3, 0, QtWidgets.QTableWidgetItem('时长'))
+        [self.table_widget.item(x, y).setTextAlignment(QtCore.Qt.AlignCenter) for x in range(4) for y in range(1)]
 
-        # TO DO：子线程更新进度条
+
+        self.download_bar = QtWidgets.QProgressBar(self.central_widget)
+        self.download_bar.setMinimum(0)
+        self.download_bar.setMaximum(100)
+        self.download_bar.setOrientation(QtCore.Qt.Horizontal)
+        self.download_bar.setObjectName('download_bar')
+
+        self.vertical_layout.addWidget(self.download_bar)
--- iFailed.py
+++ iFailed.py
@@ -2,20 +2,21 @@
 import json
 import os
 import re
-
+import time
+import sys
 import requests
 from PyQt5.QtCore import QThread, pyqtSignal, QCoreApplication
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from BaseLayout import BaseUI
 
 ts_url = []
 ts_pref_url = ''
 path = ''
 last_ac = ''
-title = ''
-up = ''
+PAUSE = 0
+CANCEL = -1
+START = 1
 ts_url_section = r',\n(.*?)\n#'
 ts_pref_url_section = r'https(.*?)segment/'
 illegal_name = r'[\/\\\:\*\?\"\<\>\|\s\n]'
@@ -27,6 +28,9 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 class GetInfoThread(QThread):
 
     signal = pyqtSignal(list)
+    no_this_ac = pyqtSignal(str)
+    ac_num_is_illegal = pyqtSignal(str)
+    same_result = pyqtSignal(str)
 
     def __init__(self):
         super(GetInfoThread, self).__init__()
@@ -37,46 +41,93 @@ class GetInfoThread(QThread):
         global last_ac
         global title
         global up
+        global create_time
+        global duration
+        global pause_point
+        global PAUSE
         input_data = self.data
-        ac_num = re.findall(r'(ac\d+)', input_data)[0]
+        if input_data is '':
+            self.no_this_ac.emit('输入为空')
+            return
+        pause_point = 0
+        PAUSE = False
+        ac_num = ''
+        temp = re.findall(r'(ac\d+)', input_data)
+        if len(temp) is 0:
+            temp = re.findall(r'(\d+)', input_data)
+            if len(temp) is 0:
+                self.ac_num_is_illegal.emit('识别不到输入的ac号')
+            else:
+                ac_num = 'ac' + temp[0]
+        else:
+            ac_num = temp[0]
         if ac_num == last_ac:
-            self.signal.emit([title, up])
+            self.signal.emit([title, up, create_time, duration])
+            self.same_result.emit('与上次输入的识别结果相同')
             return
         else:
             last_ac = ac_num
             ac_url = 'https://www.acfun.cn/v/' + ac_num
             res = requests.get(ac_url, headers=headers).text
-            data = re.findall(m3u8_section, res)[0]
+            temp = re.findall(m3u8_section, res)
+            if len(temp) is 0:
+                self.no_this_ac.emit('世界线可能发生了变动，无法找到该视频')
+                return
+            data = temp[0]
             data_json = json.loads(data)
             title = data_json['title']
             up = data_json['user']['name']
+            create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_json['videoList'][0]['uploadTime'] / 1000))
+            [m, s] = divmod(data_json['videoList'][0]['durationMillis'] / 1000, 60)
+            duration = '{:0>2d}:{:0>2d}'.format(int(m), int(s))
             ksPlay_info = data_json['currentVideoInfo']['ksPlayJson']
             ksPlay_json = json.loads(ksPlay_info)
             m3u8_file_url = ksPlay_json['adaptationSet']['representation'][0]['url']
             res = requests.get(m3u8_file_url).text
             ts_url = re.findall(ts_url_section, res)
             ts_pref_url = re.search(ts_pref_url_section, m3u8_file_url)[0]
-            info_list = [title, up]
+            info_list = [title, up, create_time, duration]
             self.signal.emit(info_list)
 
 
 class DownloadThread(QThread):
 
     signal = pyqtSignal(int)
+    pause_point = pyqtSignal(int)
+    file_exits = pyqtSignal(str)
 
     def __init__(self):
         super(DownloadThread, self).__init__()
 
     def run(self):
-        input_title = self.data
+        global path
+        input_title = self.data[0]
+        start_point = self.data[2]
         total_num = len(ts_url)
         file_name = input_title + '.mp4'
+        if os.path.exists(path + '/' + file_name) is True:
+            self.file_exits.emit('该文件已存在')
+            return
         with open(path + '/' + file_name, 'ab') as f:
-            for i in range(0, total_num):
+            for i in range(start_point, total_num):
                 ts = requests.get(ts_pref_url + ts_url[i])
                 f.write(ts.content)
                 self.signal.emit(int(100 * i / total_num))
+                if self.data[1] is PAUSE:
+                    if i == total_num:
+                        f.close()
+                        return
+                    else:
+                        self.pause_point.emit(i + 1)
+                        f.close()
+                        return
+                if self.data[1] is CANCEL:
+                    f.close()
+                    os.remove(path + '/' + file_name)
+                    self.signal.emit(0)
+                    return
             f.close()
+        self.signal.emit(100)
         # for i in range(0, total_num):
         #     ts = requests.get(ts_pref_url + ts_url[i])
         #     # with open(str.split(url_info, '?')[0], 'wb') as code:
@@ -86,27 +137,61 @@ class DownloadThread(QThread):
         #     self.signal.emit(int(100 * i / total_num))
         # file_name = title + '.mp4'
         # os.system('copy /b *.ts' + ' ' + re.sub(illegal_name, '-', file_name) + ' && ' + 'del *.ts')
-        self.signal.emit(100)
 
 
-class Implementation(QMainWindow, BaseUI):
+class AcFunDownloader(QMainWindow, BaseUI):
     def __init__(self, parent=None):
-        super(Implementation, self).__init__(parent)
+        super(AcFunDownloader, self).__init__(parent)
         self.setUI(self)
-
-        self.video_info_flag = False
+        with open('./Style.qss', 'r') as f:
+            self.setStyleSheet(f.read())
+        global path
         self.title = ''
         self.up = ''
-
+        self.duration = ''
+        self.create_time = ''
+        self.pause_point = 0
         self.get_info_thread = GetInfoThread()
         self.download_thread = DownloadThread()
-
+        path = os.getcwd()
         self.search_btn.clicked.connect(self.get_info_and_show)
         self.download_btn.clicked.connect(self.download_video)
         self.fold_btn.clicked.connect(self.get_directory)
+        self.close_btn.clicked.connect(self.close)
 
         self.get_info_thread.signal.connect(self.get_info_callback)
         self.download_thread.signal.connect(self.download_callback)
+        self.download_thread.pause_point.connect(self.store_pause_point)
+        self.download_thread.file_exits.connect(self.pop_information)
+        self.get_info_thread.no_this_ac.connect(self.pop_information)
+        self.get_info_thread.ac_num_is_illegal.connect(self.pop_information)
+        self.get_info_thread.same_result.connect(self.pop_information)
+
+    def store_pause_point(self, i):
+        self.pause_point = i
+
+    def pop_information(self, s):
+        QMessageBox.information(self, '提示', s)
+
+    def pause_task(self):
+        self.download_thread.data[1] = PAUSE
+        self.download_btn.setText(QCoreApplication.translate('GUI_MWin', '开始'))
+
+    def cancel_task(self):
+        if self.download_thread.isRunning():
+            self.download_thread.data[1] = CANCEL
+            self.download_bar.setValue(0)
+            self.download_btn.setText(QCoreApplication.translate('GUI_MWin', '下载'))
+        else:
+            os.remove(path + '/' + self.title + '.mp4')
+            self.download_bar.setValue(0)
+            self.download_btn.setText(QCoreApplication.translate('GUI_MWin', '下载'))
+        self.pause_btn.disconnect()
+        self.cancel_btn.disconnect()
+
+    def continue_task(self):
+        self.download_thread.data = [self.title, False, self.pause_point]
+        self.download_thread.start()
 
     def get_directory(self):
         global path
@@ -121,12 +206,18 @@ class Implementation(QMainWindow, BaseUI):
     def get_info_callback(self, callback_list):
         self.title = callback_list[0]
         self.up = callback_list[1]
+        self.create_time = callback_list[2]
+        self.duration = callback_list[3]
         self.table_widget.setItem(0, 1, QtWidgets.QTableWidgetItem(self.title))
         self.table_widget.setItem(1, 1, QtWidgets.QTableWidgetItem(self.up))
+        self.table_widget.setItem(2, 1, QtWidgets.QTableWidgetItem(self.create_time))
+        self.table_widget.setItem(3, 1, QtWidgets.QTableWidgetItem(self.duration))
 
-    def update_ui(self, title, up):
-        self.table_widget.setItem(0, 1, QtWidgets.QTableWidgetItem(title))
-        self.table_widget.setItem(1, 1, QtWidgets.QTableWidgetItem(up))
+    def update_ui(self, t, u, c, d):
+        self.table_widget.setItem(0, 1, QtWidgets.QTableWidgetItem(t))
+        self.table_widget.setItem(1, 1, QtWidgets.QTableWidgetItem(u))
+        self.table_widget.setItem(2, 1, QtWidgets.QTableWidgetItem(c))
+        self.table_widget.setItem(3, 1, QtWidgets.QTableWidgetItem(d))
 
     def download_callback(self, i):
         self.download_bar.setValue(i)
@@ -138,33 +229,46 @@ class Implementation(QMainWindow, BaseUI):
     def download_video(self):
         global ts_pref_url
         global ts_url
-        # if ts_pref_url is '':
-            # new_thread = GetInfoThread()
-            # new_thread.data = self.input_box.text()
-            # new_thread.signal.connect(self.get_info_callback)
-            # new_thread.start()
-            # new_thread.wait()
-        input_data = self.input_box.text()
-        ac_num = re.findall(r'(ac\d+)', input_data)[0]
-        ac_url = 'https://www.acfun.cn/v/' + ac_num
-        res = requests.get(ac_url, headers=headers).text
-        data = re.findall(m3u8_section, res)[0]
-        data_json = json.loads(data)
-        self.title = data_json['title']
-        self.up = data_json['user']['name']
-        ks_play_info = data_json['currentVideoInfo']['ksPlayJson']
-        ks_play_json = json.loads(ks_play_info)
-        m3u8_file_url = ks_play_json['adaptationSet']['representation'][0]['url']
-        res = requests.get(m3u8_file_url).text
-        ts_url = re.findall(ts_url_section, res)
-        ts_pref_url = re.search(ts_pref_url_section, m3u8_file_url)[0]
-        self.update_ui(self.title, self.up)
-        self.download_thread.data = self.title
+        if ts_pref_url is '':
+            input_data = self.input_box.text()
+            ac_num = ''
+            temp = re.findall(r'(ac\d+)', input_data)
+            if len(temp) is 0:
+                temp = re.findall(r'(\d+)', input_data)
+                if len(temp) is 0:
+                    self.ac_num_is_illegal.emit('识别不到输入的ac号')
+                else:
+                    ac_num = 'ac' + temp[0]
+            else:
+                ac_num = temp[0]
+            ac_url = 'https://www.acfun.cn/v/' + ac_num
+            res = requests.get(ac_url, headers=headers).text
+            temp = re.findall(m3u8_section, res)
+            if len(temp) is 0:
+                self.no_this_ac.emit('世界线可能发生了变动，无法找到该视频')
+                return
+            data = temp[0]
+            data_json = json.loads(data)
+            self.title = data_json['title']
+            self.up = data_json['user']['name']
+            self.create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_json['videoList'][0]['uploadTime'] / 1000))
+            [m, s] = divmod(data_json['videoList'][0]['durationMillis'] / 1000, 60)
+            self.duration = '{:0>2d}:{:0>2d}'.format(int(m), int(s))
+            ks_play_info = data_json['currentVideoInfo']['ksPlayJson']
+            ks_play_json = json.loads(ks_play_info)
+            m3u8_file_url = ks_play_json['adaptationSet']['representation'][0]['url']
+            res = requests.get(m3u8_file_url).text
+            ts_url = re.findall(ts_url_section, res)
+            ts_pref_url = re.search(ts_pref_url_section, m3u8_file_url)[0]
+            self.update_ui(self.title, self.up, self.create_time, self.duration)
+        self.download_thread.data = [self.title, START, self.pause_point]
         self.download_thread.start()
+        self.pause_btn.clicked.connect(self.pause_task)
+        self.cancel_btn.clicked.connect(self.cancel_task)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = Implementation()
+    w = AcFunDownloader()
     w.show()
     sys.exit(app.exec_())
